5273fcac5dba40c875d5950bc038f89acb20311d
NikkiMoore9114 <nikkimoore16@me.com>
2022-06-02 13:00:51 -0700

    Initial Commit

---
+++ lib/simple_form_fancy_uploads/version.rb
@@ -0,0 +1,3 @@
+module SimpleFormFancyUploads
+  VERSION = "0.1.0"
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '085214e9417cc8ba6628fc6ecf859bb1672f08480fea623da8d90a7d462cc7d349875cca920dd0777edba655f6488cf6a2bad6a4f68fe77486f06ff2b36c0fdc'
